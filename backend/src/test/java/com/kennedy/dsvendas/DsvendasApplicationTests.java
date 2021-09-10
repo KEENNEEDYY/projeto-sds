b2865caf64c7e43c3549b3921b339321a2cf9662
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2021-09-10 08:10:02 -0300

    Project Created

---
+++ backend/src/test/java/com/kennedy/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennedy.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
