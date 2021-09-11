905380d6befc431c3e3a3aff528f297a2d105277
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2021-09-11 10:13:41 -0300

    Layers

---
+++ backend/src/main/java/com/kennedy/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.kennedy.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kennedy.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
