cc3900463f99cadb268e1079006c230a548627ca
KENNEDY LEOCADIO <81121745+KEENNEEDYY@users.noreply.github.com>
2023-06-14 16:20:24 -0300

    Update footer component

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const  Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/KEENNEEDYY" target="_blank" rel="noreferrer">Kennedy Leocadio Costa</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/KEENNEEDYY" target="_blank" rel="noreferrer">Desenvolvedor Kennedy</a></p>
                 <p className="text-light"><small><strong>Spring React</strong></small></p>
             </div>
         </footer>
